7817618b2f7b48981fc838facc28a8223c1ebf16
luwenyu0905 <116932370+luwenyu0905@users.noreply.github.com>
2025-07-02 19:08:45 +0900

    Add files via upload

---
+++ dist/main.ob.js
@@ -0,0 +1,208 @@
+function _0x4c29() {
+    const _0x26ada1 = [
+        '21133iGaJCo',
+        'value',
+        'input',
+        'querySelectorAll',
+        'snkrdunk_minPrice',
+        'getElementsByTagName',
+        '?slide=view',
+        'reload',
+        'log',
+        '90iXPFVv',
+        'sizeText',
+        '/listings/',
+        'MAX_RETRY',
+        '2913228SxqUhf',
+        '✅\x20所有组最低价商品已是最优价格',
+        '⏳\x20正在提交...',
+        '❌\x20无法获取最低价',
+        'SAFE_MODE',
+        'querySelector',
+        'getItem',
+        'https://snkrdunk.com/v1/listing?page=1&perPage=24',
+        '🎯\x20发现目标:\x20当前¥',
+        '/edit/?slide=view',
+        '5003650uRcudf',
+        'PRICE_DIFF',
+        '编辑页处理失败',
+        '184472iDduOh',
+        '編集する',
+        'floor',
+        '列表页处理失败',
+        'input[name=\x22price\x22]',
+        '/apparel-listings/',
+        'BASE_DELAY_MS',
+        'textContent',
+        'error',
+        '\x20→\x20新价¥',
+        '📦\x20正在扫描商品...',
+        'minPrice',
+        '1790432WuQRRb',
+        'message',
+        'warn',
+        '✅\x20所有商品已是最优价格，30秒后重载...',
+        'setItem',
+        'alert',
+        'sort',
+        '2964797HqaiHF',
+        'href',
+        '2034UPRKdx',
+        '5610JqLlOZ',
+        '74yERGPJ',
+        'filter',
+        'dispatchEvent',
+        '⛔\x20达到最大重试次数，已停止',
+        'checkEditableUrl',
+        'find',
+        'button',
+        '✅\x20改价成功（无确认按钮）:\x20¥',
+        '✅\x20改价成功:\x20¥',
+        'json',
+        'price',
+        'click',
+        'includes',
+        '変更する',
+        'push',
+        'DELAY_RANGE_MS',
+        '36hZiHLq',
+        'https://snkrdunk.com/listing/',
+        'trim',
+        '找不到输入框或提交按钮'
+    ];
+    _0x4c29 = function () {
+        return _0x26ada1;
+    };
+    return _0x4c29();
+}
+function _0x5e7d(_0x2b55ff, _0x18daef) {
+    const _0x4c29bc = _0x4c29();
+    return _0x5e7d = function (_0x5e7d28, _0x3f7528) {
+        _0x5e7d28 = _0x5e7d28 - 0x1a5;
+        let _0x5bb2d8 = _0x4c29bc[_0x5e7d28];
+        return _0x5bb2d8;
+    }, _0x5e7d(_0x2b55ff, _0x18daef);
+}
+(function (_0x20f351, _0x366a94) {
+    const _0x34ddcd = _0x5e7d, _0x35d065 = _0x20f351();
+    while (!![]) {
+        try {
+            const _0x4e7c36 = -parseInt(_0x34ddcd(0x1b7)) / 0x1 * (-parseInt(_0x34ddcd(0x1b8)) / 0x2) + parseInt(_0x34ddcd(0x1d9)) / 0x3 + -parseInt(_0x34ddcd(0x1e6)) / 0x4 + -parseInt(_0x34ddcd(0x1e3)) / 0x5 + parseInt(_0x34ddcd(0x1b6)) / 0x6 * (-parseInt(_0x34ddcd(0x1cc)) / 0x7) + parseInt(_0x34ddcd(0x1ad)) / 0x8 * (-parseInt(_0x34ddcd(0x1c8)) / 0x9) + parseInt(_0x34ddcd(0x1d5)) / 0xa * (parseInt(_0x34ddcd(0x1b4)) / 0xb);
+            if (_0x4e7c36 === _0x366a94)
+                break;
+            else
+                _0x35d065['push'](_0x35d065['shift']());
+        } catch (_0x501dd8) {
+            _0x35d065['push'](_0x35d065['shift']());
+        }
+    }
+}(_0x4c29, 0x9bfa4), (function () {
+    'use strict';
+    const _0x54ee29 = _0x5e7d;
+    window['confirm'] = () => !![], window[_0x54ee29(0x1b2)] = () => {
+    };
+    const _0x19c14c = {
+        'BASE_DELAY_MS': 0x7d0,
+        'DELAY_RANGE_MS': 0x3e8,
+        'MAX_RETRY': 0x3,
+        'PRICE_DIFF': 0x1,
+        'SAFE_MODE': !![]
+    };
+    let _0x2340f5 = 0x0;
+    function _0x3020e2(_0x5cb8fc = _0x19c14c[_0x54ee29(0x1a7)], _0x5a4be2 = _0x19c14c[_0x54ee29(0x1c7)]) {
+        const _0x3a9eef = _0x54ee29;
+        return _0x5cb8fc + Math[_0x3a9eef(0x1e8)](Math['random']() * _0x5a4be2);
+    }
+    function _0x14b476(_0x50fb55) {
+        return new Promise(_0xa42d23 => setTimeout(_0xa42d23, _0x50fb55));
+    }
+    function _0x9ed6a7() {
+        const _0x1b870a = _0x54ee29, _0x18cda4 = document[_0x1b870a(0x1cf)]('a.button-type-1.button-collor-black');
+        return [..._0x18cda4][_0x1b870a(0x1bd)](_0x3ce16e => _0x3ce16e[_0x1b870a(0x1a8)][_0x1b870a(0x1ca)]() === 'OK') || null;
+    }
+    function _0xf93f96() {
+        const _0x330f46 = _0x54ee29, _0x3a51d3 = document[_0x330f46(0x1d1)](_0x330f46(0x1be));
+        for (const _0x1f5253 of _0x3a51d3) {
+            const _0x2c92c9 = _0x1f5253[_0x330f46(0x1a8)][_0x330f46(0x1ca)]();
+            if (_0x2c92c9 === _0x330f46(0x1c5) || _0x2c92c9 === _0x330f46(0x1e7))
+                return _0x1f5253;
+        }
+        return null;
+    }
+    async function _0x1af35d() {
+        const _0x50f4f7 = _0x54ee29, _0x54d938 = parseInt(sessionStorage[_0x50f4f7(0x1df)](_0x50f4f7(0x1d0)), 0xa);
+        if (isNaN(_0x54d938)) {
+            console[_0x50f4f7(0x1af)](_0x50f4f7(0x1dc));
+            return;
+        }
+        const _0x49e51f = _0x54d938 - _0x19c14c[_0x50f4f7(0x1e4)];
+        try {
+            const _0x5c1013 = document[_0x50f4f7(0x1de)](_0x50f4f7(0x1a5)), _0x1add4e = _0xf93f96();
+            if (!_0x5c1013 || !_0x1add4e)
+                throw new Error(_0x50f4f7(0x1cb));
+            if (_0x19c14c[_0x50f4f7(0x1dd)]) {
+                const _0x37fb45 = parseFloat(_0x5c1013[_0x50f4f7(0x1cd)]);
+                if (_0x37fb45 <= _0x49e51f)
+                    return console['log']('✅\x20当前价格已最优:\x20¥' + _0x37fb45), _0x3df507();
+            }
+            _0x5c1013[_0x50f4f7(0x1cd)] = _0x49e51f, _0x5c1013[_0x50f4f7(0x1ba)](new Event(_0x50f4f7(0x1ce), { 'bubbles': !![] })), await _0x14b476(0x320), _0x1add4e[_0x50f4f7(0x1c3)](), console[_0x50f4f7(0x1d4)](_0x50f4f7(0x1db)), await _0x14b476(0x5dc);
+            const _0x5d9500 = _0x9ed6a7();
+            _0x5d9500 ? (_0x5d9500[_0x50f4f7(0x1c3)](), console[_0x50f4f7(0x1d4)](_0x50f4f7(0x1c0) + _0x49e51f)) : console[_0x50f4f7(0x1d4)](_0x50f4f7(0x1bf) + _0x49e51f), await _0x14b476(_0x3020e2()), _0x3df507();
+        } catch (_0x3c8668) {
+            await _0xd49154(_0x3c8668, _0x50f4f7(0x1e5));
+        }
+    }
+    async function _0x1e032a() {
+        const _0x2ba42a = _0x54ee29;
+        try {
+            console[_0x2ba42a(0x1d4)](_0x2ba42a(0x1ab));
+            const _0x23905d = await fetch(_0x2ba42a(0x1e0)), _0x4324de = await _0x23905d[_0x2ba42a(0x1c1)](), _0x481187 = _0x4324de['items'][_0x2ba42a(0x1b9)](_0x2ae620 => _0x2ae620[_0x2ba42a(0x1c2)] > _0x2ae620[_0x2ba42a(0x1ac)]);
+            if (_0x481187['length'] === 0x0)
+                return console['log'](_0x2ba42a(0x1b0)), await _0x14b476(0x7530), location[_0x2ba42a(0x1d3)]();
+            const _0xb0cabe = {};
+            for (const _0x123af7 of _0x481187) {
+                const _0x57e22c = _0x123af7['productNumber'] + '|' + _0x123af7[_0x2ba42a(0x1d6)];
+                if (!_0xb0cabe[_0x57e22c])
+                    _0xb0cabe[_0x57e22c] = [];
+                _0xb0cabe[_0x57e22c][_0x2ba42a(0x1c6)](_0x123af7);
+            }
+            const _0x364269 = [];
+            for (const _0x3447d1 in _0xb0cabe) {
+                _0xb0cabe[_0x3447d1][_0x2ba42a(0x1b3)]((_0xdd286, _0xb17320) => _0xdd286[_0x2ba42a(0x1c2)] - _0xb17320[_0x2ba42a(0x1c2)]), _0x364269[_0x2ba42a(0x1c6)](_0xb0cabe[_0x3447d1][0x0]);
+            }
+            const _0x67f966 = _0x364269[_0x2ba42a(0x1bd)](_0x1ebf40 => _0x1ebf40[_0x2ba42a(0x1c2)] > _0x1ebf40[_0x2ba42a(0x1ac)]);
+            if (!_0x67f966)
+                return console['log'](_0x2ba42a(0x1da)), await _0x14b476(0x7530), location['reload']();
+            const _0x540827 = _0x67f966[_0x2ba42a(0x1ac)] - _0x19c14c[_0x2ba42a(0x1e4)];
+            console[_0x2ba42a(0x1d4)](_0x2ba42a(0x1e1) + _0x67f966[_0x2ba42a(0x1c2)] + _0x2ba42a(0x1aa) + _0x540827);
+            const _0x11078c = _0x67f966[_0x2ba42a(0x1bc)] || '';
+            let _0x381cac = '';
+            _0x11078c[_0x2ba42a(0x1c4)](_0x2ba42a(0x1a6)) ? _0x381cac = 'https://snkrdunk.com/apparel-listings/' + _0x67f966['id'] + _0x2ba42a(0x1d2) : _0x381cac = _0x2ba42a(0x1c9) + _0x67f966['id'] + _0x2ba42a(0x1e2), sessionStorage[_0x2ba42a(0x1b1)](_0x2ba42a(0x1d0), _0x67f966[_0x2ba42a(0x1ac)]), location[_0x2ba42a(0x1b5)] = _0x381cac;
+        } catch (_0x2e515c) {
+            await _0xd49154(_0x2e515c, _0x2ba42a(0x1e9));
+        }
+    }
+    async function _0x3df507() {
+        const _0x32c926 = _0x54ee29;
+        location[_0x32c926(0x1b5)] = 'https://snkrdunk.com/listings/?slide=right';
+    }
+    async function _0xd49154(_0x21e76a, _0xae1d5 = '') {
+        const _0x1a009a = _0x54ee29;
+        _0x2340f5++, console['warn']('❌\x20' + _0xae1d5 + ':\x20' + _0x21e76a[_0x1a009a(0x1ae)]);
+        if (_0x2340f5 >= _0x19c14c[_0x1a009a(0x1d8)]) {
+            console[_0x1a009a(0x1a9)](_0x1a009a(0x1bb));
+            return;
+        }
+        await _0x14b476(0xbb8), location[_0x1a009a(0x1d3)]();
+    }
+    async function _0x390c70() {
+        const _0x1d0c14 = _0x54ee29;
+        if (location[_0x1d0c14(0x1b5)][_0x1d0c14(0x1c4)](_0x1d0c14(0x1d7)))
+            await _0x1e032a();
+        else
+            (location[_0x1d0c14(0x1b5)]['includes']('/listing/') || location[_0x1d0c14(0x1b5)][_0x1d0c14(0x1c4)](_0x1d0c14(0x1a6))) && (location['href'][_0x1d0c14(0x1c4)]('/edit') || !location[_0x1d0c14(0x1b5)][_0x1d0c14(0x1c4)]('/edit')) && await _0x1af35d();
+    }
+    setTimeout(() => {
+        _0x390c70();
+    }, 0x3e8);
+}()));
